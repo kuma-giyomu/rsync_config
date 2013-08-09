d0e85ac0671c502ec4fd7a57e7a8c7c84dd5e5fa
Guillaume Bodi <bodi.giyomu@gmail.com>
2013-08-09 12:06:16 +1000

    Add base for secrets file parsing (WIP)

--- lib/rsync_config/user_management.rb
+++ lib/rsync_config/user_management.rb
@@ -2,6 +2,28 @@ module RsyncConfig
 
   module UserManagement
 
+    def self.included base
+      base.extend ClassMethods
+    end
+
+    module ClassMethods
+
+      def parse_secrets_file(content)
+        raise 'Cannot process nil' if content.nil?
+
+        Treetop.load 'lib/rsync_config/parser/secrets_file'
+        parser = RsyncSecretsFileParser.new
+        p = parser.parse content
+
+        unless p.nil?
+          return p.to_hash
+        else
+          raise RuntimeError, parser.failure_reason
+        end
+      end
+
+    end
+
     def users
       return @parent_config.users if @parent_config && @users.nil?
       local_users
@@ -29,7 +51,7 @@ module RsyncConfig
     def write_secrets_file(file)
       return if file.nil?
       raise "Cannot write secrets file #{file}" if File.exists?(file) && ! File.writable?(file)
-      raise "Cannoe create secrets file #{file}" if !File.exist?(file) && ! ( Dir.exists?(File.dirname(file)) && File.writable?(File.dirname(file)))
+      raise "Cannot create secrets file #{file}" if !File.exist?(file) && ! ( Dir.exists?(File.dirname(file)) && File.writable?(File.dirname(file)))
 
       File.open(file, 'w') do |file|
         file.write local_user_list.join "\n"
@@ -39,9 +61,15 @@ module RsyncConfig
     end
 
     def load_secrets_file(file)
-      return if file.nil?
-      raise "Cannot load secrets file #{file}" unless File.exists?(file) && File.readable?(file)
+      # fails silently if no parameter is found OR the file does not exist on disk
+      return if file.nil? || !(File.exist? file)
 
+      # fails is the file exists but cannot be read, since it's suspicious
+      raise "Cannot load secrets file #{file}" unless File.readable?(file)
+
+      File.open(file, 'r') do |file|
+        self.users = self.class.parse_secrets_file(file.read)
+      end
     end
 
     private 
