36a3231eb71ce61b7d6bd76c32bcf327199149fa
Guillaume Bodi <bodi.giyomu@gmail.com>
2013-08-09 11:31:57 +1000

    Better implementation for the crawling feature

--- lib/rsync_config/parser/node_extension.rb
+++ lib/rsync_config/parser/node_extension.rb
@@ -2,47 +2,87 @@ module RsyncConfig
 
   module Node
 
+    module Crawlable
+
+      def crawl *args
+        continue = true
+        continue = action(*args) if respond_to? :action
+
+        return if !continue || elements.nil?
+
+        elements.each do |elt|
+          elt.crawl(*args)
+        end
+      end
+
+    end
+
+    class Treetop::Runtime::SyntaxNode
+
+      include ::RsyncConfig::Node::Crawlable
+
+    end
+
+    # --------------------- CONFIG FILE NODES --------------------
+
     class Config < Treetop::Runtime::SyntaxNode
+
+      attr_accessor :config, :active_module
 
       def to_config
         @config = ::RsyncConfig::Config.new
-
         @active_module = nil
+
         crawl self 
 
         @config
       end
 
-      private 
-
-      def crawl(root)
-        return if root.elements.nil?
-        root.elements.each do |elt|
-          process_element elt
-        end
-      end
-
-      def process_element(elt)
-        case elt.class.name
-        when 'RsyncConfig::Node::Property'
-          if @active_module 
-            @active_module[elt.property.text_value] = elt.value.text_value
-          else
-            @config[elt.property.text_value] = elt.value.text_value
-          end
-        when 'RsyncConfig::Node::Module'
-          @active_module = @config.module elt.module_label.text_value
-        else
-          crawl elt
-        end
-      end
-
     end
 
     class Module < Treetop::Runtime::SyntaxNode
+
+      def action(top_node)
+        top_node.active_module = top_node.config.module(module_label.text_value)
+
+        # interrupt subtree crawling
+        false
+      end
+
     end
 
     class Property < Treetop::Runtime::SyntaxNode
+
+      def action(top_node)
+        receiver = top_node.active_module.nil? ? top_node.config : top_node.active_module
+        receiver[property.text_value] = value.text_value
+
+        # interrupt subtree crawling
+        false
+      end
+
+    end
+
+    # ------------ SECRETS FILE NODES ----------------
+
+    class Secrets < Treetop::Runtime::SyntaxNode
+
+      def to_hash
+        {}
+      end
+
+    end
+
+    class UserRecord < Treetop::Runtime::SyntaxNode
+
+      def user_value
+        user.text_value
+      end
+
+      def password_value
+        password.text_value
+      end
+
     end
 
   end
